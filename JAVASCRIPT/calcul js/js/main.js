3951fee364ae7b97f14866a87c68ffa8bd65b677
Miatti sébastien <sebastien.miatti@lepoles.com>
2017-08-08 10:43:21 +0200

    changement

---
+++ JAVASCRIPT/calcul js/js/main.js
@@ -0,0 +1,10 @@
+// commentaire de ligne
+/*----  commentaire de blocs
+sur plusieur ligne ----*/
+
+// on attend que le doc soit entièrement chargé
+window.onload = function()
+{
+    var number1 = document.getElementById("number-1");
+    console.log(number1);
+};
