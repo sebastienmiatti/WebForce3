60537834db1a45b8778921dfa2c530f945fa2575
Miatti sébastien <sebastien.miatti@lepoles.com>
2017-08-21 16:58:50 +0200

    gf

--- JAVASCRIPT/JQuery/Shadow Box/js/main.js
+++ JAVASCRIPT/JQuery/Shadow Box/js/main.js
@@ -1,25 +1,25 @@
 $(function(){
-    var sliders = $(".slider-wrapper");
+    var sliders = $("#thumbnail-wrapper");
 
     sliders.each(function(){
         //pour chaques éléments contenu dans le tableau sliders
-        var container = $(this).find(".slide-container"); // contient l'enfant .slide-container du slider
+        var container = $(this).find("#thumbnail-wrapper"); // contient l'enfant .slide-container du slider
 
         var slides = $(this).find(".slide"); // contient la liste des éléments .slide qui sont dans le sliders
 
-        var containerWidth = slides.length * 600;
+        var containerWidth = slides.length * 300;
 
         var isMoving = false; // variable de blocage pour savoir si l'animation est en cours ou non
 
         container.css("width", containerWidth);// initialisation du slider a 0
 
         // on ajoute les écouteurs d'évènements pour gérer la navigation
-        $(this).find(".btn-prev").on("click", function(e){
+        $(this).find("#thumbnail-wrapper").on("click", function(e){
             e.preventDefault();
             //alert('backward');
             if(container.position().left < 0 && !isMoving){
                 isMoving = true; //l'animation commence
-            container.animate({"left": "+=600"}, function(){
+            container.animate({"left": "+=300"}, function(){
                 //fonction de callback de la fonction animate
                 //alert('fini');
                 isMoving = false; // l'animation est terminée
@@ -28,12 +28,12 @@ $(function(){
         }
         });
 
-        $(this).find(".btn-next").on("click", function(e){
+        $(this).find("btn-next").on("click", function(e){
             e.preventDefault();
             //alert('forward');
-            if(container.position().left > -(slides.length-1)*400 && !isMoving){
+            if(container.position().left > -(slides.length-1)*300 && !isMoving){
                 isMoving = true;
-            container.animate({"left": "-=600"},  function(){
+            container.animate({"left": "-=300"},  function(){
                 //fonction de callback de la fonction animate
                 //alert('fini');
                 isMoving = false;
