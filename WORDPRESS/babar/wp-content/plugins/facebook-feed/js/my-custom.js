85bfaee2ef309a5cf29d20ef1ee40d4a7bc822f1
Unknown <sebastien.miatti@lepoles.com>
2017-09-04 16:58:05 +0200

    ghf

--- WORDPRESS/babar/wp-content/plugins/facebook-feed/js/my-custom.js
+++
@@ -1,9 +0,0 @@
-jQuery(function($) {
-    $( "#option-tree-settings-api" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
-    $( "#option-tree-settings-api li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
-  });
-  
-  
-  jQuery(function($) {
-    $('.wff-color-picker-panel').wpColorPicker();
-  });
